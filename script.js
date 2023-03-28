068646f9275e76a39c1b9694ed24ace2f7db2e9b
samfloyd9 <floyd.samuel.23@gmail.com>
2023-03-27 21:24:40 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function setDisplayVisible(id) {
+  const visibleElement = document.getElementById(id);
+  visibleElement.style.display = "block";
+}
+
+function setDisplayHidden(id) {
+  const hiddenElement = document.getElementById(id);
+  hiddenElement.style.display = "none";
+}
